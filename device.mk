0844e634a48c51904617ce817221fb0afc772460
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    surya: Allow to access bootable/deprecated-ota targets
    surya is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to surya namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: HinohArata <mmgcntk@gmail.com>
    Signed-off-by: HinohArata <161218134+HinohArata@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -464,6 +464,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     firmware/xiaomi/surya \
     vendor/qcom/opensource/usb/etc \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
