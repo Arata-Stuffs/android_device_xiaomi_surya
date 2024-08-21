ef0907ad2facea4129998f604bbb5121f112bcc5
HinohArata <mmgcntk@gmail.com>
2024-08-21 19:36:00 +0700

    surya: Build GalleryGo if exist
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -39,6 +39,9 @@ $(call inherit-product, device/xiaomi/surya/Gallery/config.mk)
 # Kernel vintf
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
+# GalleryGo
+$(call inherit-product-if-exists, vendor/prebuilts/GalleryGO/config.mk)
+
 # Signing
 -include vendor/private-keys/keys/keys.mk
 
