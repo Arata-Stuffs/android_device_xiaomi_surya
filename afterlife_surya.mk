93281430ac499274f00b303d063a21f251f5649e
HinohArata <mmgcntk@gmail.com>
2024-11-28 14:33:11 +0700

    afterlife: Update flags
    Change-Id: I9e4b3dc716f6000efb25861fd64dd5084be0b460
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- afterlife_surya.mk
+++ afterlife_surya.mk
@@ -32,6 +32,9 @@ TARGET_SUPPORTS_BLUR := true
 # AfterEcho
 TARGET_USE_ECHO := true
 
+# LawnIcons
+TARGET_PREBUILTS_LAWNICONS := true
+
 # Device Manufacturer
 PRODUCT_NAME := afterlife_surya
 PRODUCT_DEVICE := surya
