557fd4548cb381df8a35cb65d5249b2d15659c4a
HinohArata <mmgcntk@gmail.com>
2025-01-19 19:13:18 +0700

    surya: Shim widevine with libcrypto_shim
    Change-Id: Iba6074e7e25669667451cf2863dd2c61c3880211
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -66,11 +66,17 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
-    vendor/lib64/hw/fingerprint.goodix.default.so)
-        [ "$2" = "" ] && return 0
-        "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix.default.so" "${2}"
-        ;;
-        *)
+
+        vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed  "libcrypto_shim.so" "${2}"
+            ;;
+
+        vendor/lib64/hw/fingerprint.goodix.default.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix.default.so" "${2}"
+            ;;
+            *)
             return 1
             ;;
     esac
